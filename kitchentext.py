104e6d8daba9677ed816b1bf46a228aaa989e298
Shy <shy@koeln.ccc.de>
2017-04-14 17:21:04 +0200

    Added license information

--- kitchentext.py
+++ kitchentext.py
@@ -3,6 +3,19 @@
 # kitchentext: Read text from stdin and put it on the Kitchenlight.
 #
 # Author: Shy
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 def kitchentext(delay=200, single=False, wait=False, restore=False,
                 poweron=False, verbose=False, debug=False):
