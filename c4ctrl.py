78dfd88dfa5fc936a43fc58593c36669abec70f4
Shy <shy@koeln.ccc.de>
2017-04-14 17:20:41 +0200

    Added license information

--- c4ctrl.py
+++ c4ctrl.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
-#
+#                 \__/
+#                 /
+#         _______/
+#        /   ____\__
+#__     /   / __// / 
+#  \___/   / /  / /___  ____
+#__/   \   \ \__\_   / / __/____ __ _ 
+#       \   \___\ |_| / / |_   _|  \ |
+#        \_______/    \ \__ | ||   / |_
+#                \     \___\|_||_\_\___\
+#                 \__
+#                 /  \  c4ctrl: A command line
+#                 \__/  client for Autoc4
 # Author: Shy
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 A command line client for Autoc4, the home automation system of the C4.
